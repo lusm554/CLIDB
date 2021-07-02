1f1c886574efd9799e1dfa890d8233e16aec7e2f
loveyousomuch <loveyousomuch554@gmail.com>
2021-07-02 13:12:33 +0300

    added indents

--- dao/files.py
+++ dao/files.py
@@ -4,45 +4,45 @@ from datetime import datetime
 
 # Utils
 def path(filename):
-  return os.path.join('var', filename)
+     return os.path.join('var', filename)
 
 def to_human_time(time):
-  return str(datetime.fromtimestamp(time))
+    return str(datetime.fromtimestamp(time))
 
 # CRUD functions
 def add(file, data):
-  with open(path(file), 'w') as outfile:
-    json.dump(data, outfile)
+    with open(path(file), 'w') as outfile:
+        json.dump(data, outfile)
 
 def delete(filename):
-  os.remove(path(filename))
+    os.remove(path(filename))
 
 def put(filename, new_data):
-  old_data = ''
-  current = {}
-  with open(path(filename)) as json_file:
-    old_data = json.load(json_file)
-  for key in old_data.keys():
-    if key in new_data:
-      current[key] = new_data[key]
-    else:
-      current[key] = old_data[key]
-  with open(path(filename), 'w') as outfile:
-    json.dump(current, outfile)
+    old_data = ''
+    current = {}
+    with open(path(filename)) as json_file:
+        old_data = json.load(json_file)
+    for key in old_data.keys():
+        if key in new_data:
+            current[key] = new_data[key]
+        else:
+            current[key] = old_data[key]
+    with open(path(filename), 'w') as outfile:
+        json.dump(current, outfile)
 
 def get(filename):
-  with open(path(filename)) as json_file:
-    data = json.load(json_file)
-    return data
+    with open(path(filename)) as json_file:
+        data = json.load(json_file)
+        return data
 
 def info():
-  data = {}
-  files = os.listdir('var')
-  for file in files:
-    data[file] = {
-      'size': '{0} bytes'.format(os.path.getsize(path(file))),
-      'permission': oct(os.stat(path(file)).st_mode)[-3:],
-      'created:': to_human_time(os.path.getctime(path(file))),
-      'modified': to_human_time(os.path.getmtime(path(file)))
-    }
-  return data
+    data = {}
+    files = os.listdir('var')
+    for file in files:
+        data[file] = {
+            'size': '{0} bytes'.format(os.path.getsize(path(file))),
+            'permission': oct(os.stat(path(file)).st_mode)[-3:],
+            'created:': to_human_time(os.path.getctime(path(file))),
+            'modified': to_human_time(os.path.getmtime(path(file)))
+        }
+    return data
--- main.py
+++ main.py
@@ -9,118 +9,118 @@ print(logo)
 
 # Create dir for files if not exist
 try:
-  if not os.path.exists('var'):
-    os.makedirs('var')
+    if not os.path.exists('var'):
+        os.makedirs('var')
 except OSError as e:
-  if e.errno != errno.EEXIST:
-    raise
+    if e.errno != errno.EEXIST:
+        raise
 
 def add_file():
-  file_name = input('File name: ')
-  if file_name == '': return cprint('Value field cannot be empty.\n', 'red', attrs=['underline'])
-  data = {
-    'name': None,
-    'age': None,
-    'class': None
-  }
-  for key in data.keys():
-    input_data = input('{0}: '.format(key))
-    if input_data == '':
-      cprint('Value field cannot be empty.\n', 'red', attrs=['underline'])
-      return
-    data[key] = input_data
-  add(file_name, data)
-  cprint('\nFile {0} writen\n'.format(file_name), 'green', attrs=['underline'])
+    file_name = input('File name: ')
+    if file_name == '': return cprint('Value field cannot be empty.\n', 'red', attrs=['underline'])
+    data = {
+        'name': None,
+        'age': None,
+        'class': None
+    }
+    for key in data.keys():
+        input_data = input('{0}: '.format(key))
+        if input_data == '':
+            cprint('Value field cannot be empty.\n', 'red', attrs=['underline'])
+            return
+        data[key] = input_data
+    add(file_name, data)
+    cprint('\nFile {0} writen\n'.format(file_name), 'green', attrs=['underline'])
 
 def get_file():
-  file_name = input('File name: ')
-  if file_name == '': return cprint('Value field cannot be empty.\n', 'red', attrs=['underline'])
-  if not os.path.exists(os.path.join('var', file_name)):
-    return cprint('File not found.\n', 'red', attrs=['underline'])
-  print()
-  data = get(file_name)
-  for key in data.keys():
-    print('{0}: {1}'.format(key, data[key]))
-  print()
+    file_name = input('File name: ')
+    if file_name == '': return cprint('Value field cannot be empty.\n', 'red', attrs=['underline'])
+    if not os.path.exists(os.path.join('var', file_name)):
+        return cprint('File not found.\n', 'red', attrs=['underline'])
+    print()
+    data = get(file_name)
+    for key in data.keys():
+        print('{0}: {1}'.format(key, data[key]))
+    print()
 
 def info_files():
-  files = info()
-  if len(files) == 0: return cprint('Files not found.\n', 'red')
-  for file in files.keys():
-    print('{0}'.format(file))
-    for info_key in files[file].keys():
-      print('  {0}: {1}'.format(info_key, files[file][info_key]))
-  print()
+    files = info()
+    if len(files) == 0: return cprint('Files not found.\n', 'red')
+    for file in files.keys():
+        print('{0}'.format(file))
+        for info_key in files[file].keys():
+            print('  {0}: {1}'.format(info_key, files[file][info_key]))
+    print()
 
 def delete_files():
-  file_name = input('File name: ')
-  if file_name == '': return cprint('Value field cannot be empty.\n', 'red', attrs=['underline'])
-  if not os.path.exists(os.path.join('var', file_name)):
-    return cprint('File not found.\n', 'red', attrs=['underline'])
-  delete(file_name)
-  cprint('File deleted.', 'green')
+    file_name = input('File name: ')
+    if file_name == '': return cprint('Value field cannot be empty.\n', 'red', attrs=['underline'])
+    if not os.path.exists(os.path.join('var', file_name)):
+        return cprint('File not found.\n', 'red', attrs=['underline'])
+    delete(file_name)
+    cprint('File deleted.', 'green')
 
 def change_file():
-  file_name = input('File name: ')
-  if file_name == '': return cprint('Value field cannot be empty.\n', 'red', attrs=['underline'])
-  current = {}
-  if not os.path.exists(os.path.join('var', file_name)):
-    return cprint('File not found.\n', 'red', attrs=['underline'])
-  print()
-  data = get(file_name)
-  for key in data.keys():
-    print('{0}: {1}'.format(key, data[key]))
-  key = input('Which key you want change? ')
-  if not key in data:
-    return cprint('Key does not exist.', 'red')
-  print()
-  current[key] = input('Value for {0}: '.format(key))
-  put(file_name, current)
-  cprint('File successul changed.', 'green')
+    file_name = input('File name: ')
+    if file_name == '': return cprint('Value field cannot be empty.\n', 'red', attrs=['underline'])
+    current = {}
+    if not os.path.exists(os.path.join('var', file_name)):
+        return cprint('File not found.\n', 'red', attrs=['underline'])
+    print()
+    data = get(file_name)
+    for key in data.keys():
+        print('{0}: {1}'.format(key, data[key]))
+    key = input('Which key you want change? ')
+    if not key in data:
+        return cprint('Key does not exist.', 'red')
+    print()
+    current[key] = input('Value for {0}: '.format(key))
+    put(file_name, current)
+    cprint('File successul changed.', 'green')
 
 commands = {
-  '1': {
-    'info': 'Get item',
-    'action': get_file
-  },
-  '2': {
-    'info': 'Delete item',
-    'action': delete_files
-  },
-  '3': {
-    'info': 'Add item',
-    'action': add_file
-  },
-  '4': {
-    'info': 'Info about items',
-    'action': info_files
-  },
-  '5': {
-    'info': 'Change element in db',
-    'action': change_file
-  },
-  '6':  {
-    'info': 'Clear terminal',
-    'action': lambda: os.system('clear')
-  },
-  '7': {
-    'info': 'Exit',
-    'action': lambda code=0: cprint('Bye, have a nice day!', 'green') or exit(code)
-  } 
+    '1': {
+        'info': 'Get item',
+        'action': get_file
+    },
+    '2': {
+        'info': 'Delete item',
+        'action': delete_files
+    },
+    '3': {
+        'info': 'Add item',
+        'action': add_file
+    },
+    '4': {
+        'info': 'Info about items',
+        'action': info_files
+    },
+    '5': {
+        'info': 'Change element in db',
+        'action': change_file
+    },
+    '6':  {
+        'info': 'Clear terminal',
+        'action': lambda: os.system('clear')
+    },
+    '7': {
+        'info': 'Exit',
+        'action': lambda code=0: cprint('Bye, have a nice day!', 'green') or exit(code)
+    } 
 }
 
 def log_cmd():
-  info = ''
-  for (k, v) in commands.items():
-    info += '{0}. {1}\n'.format(k, v['info']) 
-  print(info)
+    info = ''
+    for (k, v) in commands.items():
+        info += '{0}. {1}\n'.format(k, v['info']) 
+    print(info)
   
 while 1:   
-  log_cmd()
-  cmd = input('cmd? ')
-  if cmd not in commands:
-    cprint('Command \'{0}\' not found.\n'.format(cmd), 'red', attrs=['underline'])
-    continue
-  commands[cmd]['action']() 
-  print('-'*50)
+    log_cmd()
+    cmd = input('cmd? ')
+    if cmd not in commands:
+        cprint('Command \'{0}\' not found.\n'.format(cmd), 'red', attrs=['underline'])
+        continue
+    commands[cmd]['action']() 
+    print('-'*50)
 
